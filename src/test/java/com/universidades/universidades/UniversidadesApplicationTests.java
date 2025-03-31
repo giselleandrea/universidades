79390d134b95f19caafe615d3239de1db373c0c0
giselleandrea <giselle.andrea90@gmail.com>
2025-03-31 10:03:40 -0500

    primer commit

---
+++ src/test/java/com/universidades/universidades/UniversidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidades.universidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
